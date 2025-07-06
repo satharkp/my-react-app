f92fc10c914951a45dfb4fe5bf1d4670c110eda6
Sathar KP <satharkp292@google.com>
2025-07-06 19:13:44 +0530

    changed merged on now its working

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-react-app/',
+  base: '/my-react-app/', // ← must match repo name
   plugins: [react()],
-})
+});
