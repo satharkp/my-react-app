a30b83cb713f3448272f6dc445ffd0a4c29f66ea
Sathar KP <satharkp292@google.com>
2025-07-06 18:49:41 +0530

    changed merged on now its working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import Todolist from "./Todolist"
 
 function App() {
@@ -11,28 +11,3 @@ function App() {
 }
 
 export default App
-=======
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-
-function App() {
-  return (
-    <BrowserRouter>
-    <nav>
-      <Link to='/'>HOME |</Link> 
-      <Link to='/home'> ABOUT |</Link> 
-      <Link to='/contact'> CONTACT</Link>
-    </nav>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/home' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-    </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
->>>>>>> 65687eabfaaa5de9f6fd81f55c91afd652a43a6f
